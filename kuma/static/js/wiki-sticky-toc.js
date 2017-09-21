8f18a55367086076c2bb42c4bff5ea26669f3fb3
Stephanie Hobson <sjhobson@gmail.com>
2017-09-21 21:57:27 +0100

    StickyTOC prototype

---
+++ kuma/static/js/wiki-sticky-toc.js
@@ -0,0 +1,39 @@
+(function ($, win, doc) {
+    'use strict';
+
+    // list of TOC items
+    var tocLinks = ['Syntax', 'Description', 'Constructor', 'Properties', 'Methods', 'Examples', 'Example', 'Live examples', 'Specification', 'Specifications', 'Browser Compatibility', 'Browser compatibility', 'See also']
+
+    // is this a docs page?
+    var isDocsPage = location.pathname.toString().indexOf('/en-US/docs/') === 0 ? true : false;
+    if(!isDocsPage) {
+        return;
+    }
+
+    // hide toc
+    $('#toc').toggle();
+
+    // attach the sticky TOC to the document
+    var $stickyTOC = $('<div id="stickyTOC"></div>');
+    var $center = $('<div class="center"></div>');
+    $center.appendTo($stickyTOC);
+
+    $(tocLinks).each(function(index) {
+        var slug = (tocLinks[index].toString()).replace(' ', '_');
+        if($('h2#' + slug).length){
+            $('<a href="#' + slug + '">' + tocLinks[index] + '</a>').appendTo($center);
+        }
+    });
+    $stickyTOC.insertAfter('#wiki-document-head');
+
+    if($('#stickyTOC a').length > 0) {
+        // add TOC
+        $('body').addClass('hasStickyTOC');
+    } else {
+        $stickyTOC.remove();
+    }
+
+
+
+
+}(jQuery, window, document));
